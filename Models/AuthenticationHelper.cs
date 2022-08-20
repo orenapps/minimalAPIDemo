b3f84f712fb8709047334872cc58bdd46d85d16f
orenfe <OrenFe@bezeq.co.il>
2022-08-20 14:13:00 +0300

    first commit

---
+++ Models/AuthenticationHelper.cs
@@ -0,0 +1,35 @@
+
+public class AuthenticationHelper
+{
+
+    Microsoft.Extensions.Configuration.ConfigurationManager _configuration;
+    public AuthenticationHelper(Microsoft.Extensions.Configuration.ConfigurationManager configuration)
+    {
+        _configuration = configuration;
+    }
+
+    public string? Login(LoginVM loginVM)
+    {
+        if (loginVM.Username == "admin" && loginVM.Password == "password")
+        {
+            return GenerateJWT();
+        }
+        else
+            return null;
+    }
+
+    private string GenerateJWT()
+    {
+        var secret = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:SigningKey"]));
+        var credentials = new SigningCredentials(secret, SecurityAlgorithms.HmacSha256);
+
+        var token = new JwtSecurityToken(
+        issuer: _configuration["Jwt:ValidIssuer"],
+        audience: _configuration["Jwt:ValidAudiance"],
+        claims: null,
+        expires: System.DateTime.Now.AddMinutes(120),
+        signingCredentials: credentials);
+
+        return new JwtSecurityTokenHandler().WriteToken(token);
+    }
+}
